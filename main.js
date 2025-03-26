af5d34c9c0c30d3f650eedd3366644ad74bff5c2
EstebanCaponio <estebancaponio@gmail.com>
2025-03-26 15:25:52 +0100

    feat: first milestone

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('test');
+const express = require('express');
+const app = express();
+const port = 3005;
+
+app.get('/', (req, res)=>{
+    res.send('Server del mio blog');
+    console.log('ciao');
+})
+
+app.listen(port, ()=>{
+    console.log('messaggio in terminale vs code');
+})
+
+// - Creiamo il progetto base con una rotta / che ritorna un testo semplice con scritto ”Server del mio blog”
+
+// - Creiamo un array dove inserire una lista di almeno 5 post, per ognuno indicare titolo, contenuto, immagine e tags (tags è un array di stringhe)
+
+// - Creiamo poi una rotta /bacheca che restituisca un oggetto json con la lista dei post.
+
+// - Configuriamo gli asset statici sull’applicazione in modo che si possano visualizzare le immagini associate ad ogni post.
+
+// - Testare su postman;
